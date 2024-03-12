82e400fdd84cd6c3def9176913c5639357cc3416
Gatchan1 <r.barrio.f@gmail.com>
2024-03-12 19:14:48 +0100

    added update story routes

--- routes/stories.routes.js
+++ routes/stories.routes.js
@@ -19,9 +19,60 @@ router.post("/", (req, res, next) => {
   Story.create(newStory)
     .then((response) => res.json(response))
     .catch((err) => {
-      console.log("Error while creating the project", err);
-      res.status(500).json({ message: "Error while creating the project" });
+      console.log("Error while creating the story", err);
+      res.status(500).json({ message: "Error while creating the story" });
     });
 });
+
+// PATCH /stories/:storyId/body  -  Update a story (body)
+router.patch("/:storyId/body", (req, res, next) => {
+  const {storyId} = req.params;
+  const { body, tags } = req.body;
+  const updatedStory = { body, tags };
+  Story.findByIdAndUpdate(storyId, updatedStory)
+    .then((response) => res.json(response))
+    .catch((err) => {
+      console.log("Error while updating the story", err);
+      res.status(500).json({ message: "Error while updating the story" });
+    });
+})
+
+// (A PATCH route for a tickbox that swaps between private/public vvvvv)
+// PATCH /stories/:storyId/privacity  -  Update a story (privacity)
+router.patch("/:storyId/privacity", (req, res, next) => {
+  const {storyId} = req.params;
+  const { private } = req.body;
+  const updatedStory = { private };
+  Story.findByIdAndUpdate(storyId, updatedStory)
+    .then((response) => res.json(response))
+    .catch((err) => {
+      console.log("Error while updating the story", err);
+      res.status(500).json({ message: "Error while updating the story" });
+    });
+})
+
+// PATCH /stories/:storyId/uuid  -  Update a story (uuid)
+router.patch("/:storyId/uuid", (req, res, next) => {
+  // Will create or destroy a uuid.
+  // I mean if you're a logged in user and the story is set to private,
+  // then the "shareability" is up to whether or not the uuid exists or not.
+  const {storyId} = req.params;
+
+  Story.findById(storyId)
+  .then((story) => {
+    if (!story.uuid) {
+      const uuid = uuidv4();
+      return Story.findByIdAndUpdate(storyId, {uuid})
+    } else {
+      return Story.findByIdAndUpdate(storyId, {uuid: ""})
+    }
+  })
+  .then((resp) => res.json(resp))
+  .catch((err) => {
+    console.log("Error while managing the share link", err);
+    res.status(500).json({ message: "Error while managing the share link" });
+  });
+})
+
 
 module.exports = router;
